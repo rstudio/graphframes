455389fa1bffa49c30de4fcb0eea16201655b2a6
kevinykuo <kevinykuo@gmail.com>
2018-03-28 00:36:52 -0400

    update examples tests outputs

--- tests/testthat/test-gf-examples.R
+++ tests/testthat/test-gf-examples.R
@@ -5,8 +5,7 @@ test_requires("dplyr")
 
 test_that("gf_star() works", {
   expect_known_output(
-    gf_star(sc, n = 10) %>%
-      glimpse(),
+    gf_star(sc, n = 10),
     "output/gf_star.txt",
     print = TRUE
   )
@@ -14,8 +13,7 @@ test_that("gf_star() works", {
 
 test_that("gf_chain() works", {
   expect_known_output(
-    gf_chain(sc, n = 5) %>%
-      glimpse(),
+    gf_chain(sc, n = 5),
     "output/gf_chain.txt",
     print = TRUE
   )
@@ -32,5 +30,13 @@ test_that("gf_grid_ising_model() works", {
     grid_ising_model %>%
       gf_vertex_columns(),
     c("i", "j", "id", "a")
+  )
+})
+
+test_that("gf_two_blobs() works", {
+  expect_known_output(
+    gf_two_blobs(sc, 2),
+    "output/two_blobs.txt",
+    print = TRUE
   )
 })

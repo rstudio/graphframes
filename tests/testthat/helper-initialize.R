82baecc14e5d5827e13bafd1c5baffcdf04f96ab
kevinykuo <kevinykuo@gmail.com>
2018-03-28 11:15:01 -0400

    qualify sparkly function calls in test initialization

--- tests/testthat/helper-initialize.R
+++ tests/testthat/helper-initialize.R
@@ -7,30 +7,30 @@
 testthat_spark_connection <- function() {
   version <- Sys.getenv("SPARK_VERSION", unset = "2.1.0")
 
-  spark_installed <- spark_installed_versions()
+  spark_installed <- sparklyr::spark_installed_versions()
   if (nrow(spark_installed[spark_installed$spark == version, ]) == 0) {
     options(sparkinstall.verbose = TRUE)
-    spark_install(version)
+    sparklyr::spark_install(version)
   }
 
-  expect_gt(nrow(spark_installed_versions()), 0)
+  expect_gt(nrow(sparklyr::spark_installed_versions()), 0)
 
   # generate connection if none yet exists
   connected <- FALSE
   if (exists(".testthat_spark_connection", envir = .GlobalEnv)) {
     sc <- get(".testthat_spark_connection", envir = .GlobalEnv)
-    connected <- connection_is_open(sc)
+    connected <- sparklyr::connection_is_open(sc)
   }
 
   if (!connected) {
-    config <- spark_config()
+    config <- sparklyr::spark_config()
 
     options(sparklyr.sanitize.column.names.verbose = TRUE)
     options(sparklyr.verbose = TRUE)
     options(sparklyr.na.omit.verbose = TRUE)
     options(sparklyr.na.action.verbose = TRUE)
 
-    sc <- spark_connect(master = "local", version = version, config = config)
+    sc <- sparklyr::spark_connect(master = "local", version = version, config = config)
     assign(".testthat_spark_connection", sc, envir = .GlobalEnv)
   }
 

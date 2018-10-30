3e5ec2ced631c5fd8b69ba1bfa99dae6a60feaaf
kevinykuo <kevinykuo@gmail.com>
2018-10-30 04:00:01 -0400

    update graphframes dependency to 0.6

--- R/dependencies.R
+++ R/dependencies.R
@@ -2,7 +2,7 @@ spark_dependencies <- function(spark_version, scala_version, ...) {
   spark_dependency(
     jars = NULL,
     packages = c(
-      sprintf("graphframes:graphframes:0.5.0-spark%s-s_%s", spark_version, scala_version)
+      sprintf("graphframes:graphframes:0.6.0-spark%s-s_%s", spark_version, scala_version)
     )
   )
 }

dd7e5985b1702d0c91ef558b15c890f21221f1ad
Jonas <exoulster@gmail.com>
2021-10-21 14:32:16 +0800

    support spark 3.1

--- R/dependencies.R
+++ R/dependencies.R
@@ -1,5 +1,9 @@
 spark_dependencies <- function(spark_version, scala_version, ...) {
-  graphframes_version <- if (spark_version >= "2.4.0") {
+  graphframes_version <- if (spark_version >= "3.1.0") {
+    "0.8.2"
+  } else if (spark_version >= "3.0.0") {
+    "0.8.0"
+  } else if (spark_version >= "2.4.0") {
     "0.7.0"
   } else if (spark_version >= "2.2.0") {
     "0.6.0"
